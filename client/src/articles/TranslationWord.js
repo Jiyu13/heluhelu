5337b5691c3f3e8d6192586d17cf622cae68ea6b
Ziru Zou <jiyucoding@yahoo.com>
2023-04-17 12:04:20 -0400

    toggle read more/less translation

--- client/src/articles/TranslationWord.js
+++ client/src/articles/TranslationWord.js
@@ -1,16 +1,46 @@
+import { useState } from "react"
 import styled from "styled-components"
 
 
 export function TranslationWord( {word} ) {
 
+    const [isReadMore, setReadMore] = useState(false)
+
+
+    // const toggleReadMore = () => {
+    //     setReadMore(!isReadMore)
+    // }
+    
+
     return (
         <WordItem key={word.id}>
             <Word>{word.hawaiian} :</Word>
-            <Translation>{word.translation}</Translation>
+            {/* <Translation>{word.translation}</Translation> */}
+            <Translation>
+                {word.translation.length > 50 ? word.translation.substring(0, 50) : word.translation}
+
+                {isReadMore ?  word.translation : word.translation.substring(0, 50)}
+
+                {word.translation.length > 50 ? 
+                
+                    <ReadOrHidebutton onClick={() => setReadMore(!isReadMore)}>
+                        {isReadMore ? " Show Less" : "Show More"}
+                    </ReadOrHidebutton> 
+                    : null
+                }
+                
+
+                
+            </Translation>
             <hr/>
         </WordItem>
     )
 }
+
+const ReadOrHidebutton = styled.button`
+    // color: rgb(192,192,192);
+    cursor: pointer;
+`
 
 const WordItem = styled.div``
 
@@ -19,4 +49,7 @@ const Word = styled.div`
     color: rgb(255, 255, 255);
 `
 
-const Translation = styled.div``
+const Translation = styled.div`
+    display: inline;
+  width: 100%;
+`
