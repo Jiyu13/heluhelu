0943f9e42f9f0743594080516eae6ea87c46fa3d
Ziru Zou <jiyucoding@yahoo.com>
2023-05-10 16:25:07 -0400

    Update CORS

--- server/config.py
+++ server/config.py
@@ -11,8 +11,9 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_COOKIE_DOMAIN'] = ".heluhe.lu"
 app.config['CORS_ALLOW_HEADERS'] = ['Content-Type']
-app.config['CORS_ORIGINS'] = ['https://heluhe.lu']
+app.config['CORS_ORIGINS'] = ["https://heluhe.lu"]
 app.json.compact = False
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 
@@ -21,7 +22,7 @@ metadata = MetaData(naming_convention={
 })
 db = SQLAlchemy(metadata=metadata)
 
-CORS(app)
+CORS(app, supports_credentials=True)
 migrate = Migrate(app, db)
 db.init_app(app)
 
