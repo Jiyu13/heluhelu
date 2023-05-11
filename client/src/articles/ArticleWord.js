5df6c73b708e2b0ccd1bc7a86512b96e67d801fd
Ziru Zou <jiyucoding@yahoo.com>
2023-05-11 15:18:04 -0400

    add margin-bottom for word, change padding

--- client/src/articles/ArticleWord.js
+++ client/src/articles/ArticleWord.js
@@ -44,9 +44,9 @@ export function ArticleWord({ word, onWordClicked, setWordExistError }) {
 const WordContainer = styled.div`
     border-radius: 5px;
     margin-right: 10px;
-    padding: 2px 4px;
+    margin-bottom: 8px;
+    padding: 0px 4px;
     display: inline-block;
-    // background: rgba(50,100,150,.6)!important;
     vertical-align: top;
     font-size: 20px;
     &:hover {
