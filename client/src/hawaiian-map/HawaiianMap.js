a0c8436358e77d538a96874eabe99f15b4feffc5
Ziru Zou <jiyucoding@yahoo.com>
2023-05-13 14:55:05 -0400

    style HawaiianMap

--- client/src/hawaiian-map/HawaiianMap.js
+++ client/src/hawaiian-map/HawaiianMap.js
@@ -5,39 +5,40 @@ const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/co
 
 export function HawaiianMap() {
     return (
-        <ComposableMap
-            projection={"geoAlbers"}
-            height={325}
-            width={320}
-            projectionConfig={{ 
-                center: [-59, -342], 
-                scale: 1500,
-                // rotate: [-10.0, -53.0, 0]
-            }}
-            
-        >
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
+        <div style={{"overflowY": "none"}}>
+            <ComposableMap
+                projection={"geoAlbers"}
+                height={180}
+                width={180}
+                projectionConfig={{ 
+                    center: [-93, -328.5], 
+                    scale: 1000,
+                    rotate: [93, 60.0, 0]
+                }}>
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map((geo) => (
+                            <Geography key={geo.rsmKey} geography={geo} />
+                    ))
+                    }
+                </Geographies>
+            </ComposableMap>
+
+    
+    {/* <ComposableMap>
+
+        <ZoomableGroup center={[-150, 0]} disablePanning>
+            <Geographies geography={geoUrl}>
+                {({ geographies }) =>
               geographies.map((geo) => (
                 <Geography key={geo.rsmKey} geography={geo} />
               ))
             }
           </Geographies>
-        </ComposableMap>
-
-    
-    // <ComposableMap>
-
-    //     <ZoomableGroup center={[-150, -150]} disablePanning>
-    //         <Geographies geography={geoUrl}>
-    //         {({ geographies }) =>
-    //           geographies.map((geo) => (
-    //             <Geography key={geo.rsmKey} geography={geo} />
-    //           ))
-    //         }
-    //       </Geographies>
         
-    //     </ZoomableGroup>
-    // </ComposableMap>
-      )
+        </ZoomableGroup>
+    </ComposableMap> */}
+    </div>
+    )
+
 }
