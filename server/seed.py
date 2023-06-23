1efd4291eb34d22ae0c251766c16a7bd86c37ec4
Ziru Zou <jiyucoding@yahoo.com>
2023-06-23 00:39:28 -0400

    comment out all the make_user()

--- server/seed.py
+++ server/seed.py
@@ -3,82 +3,82 @@ from app import app
 import datetime
 import uuid
 
-def make_user():
-    User.query.delete()
-    Article.query.delete()
-    UserWord.query.delete()
-    PageReadEvent.query.delete()
-    Vocabulary.query.delete()
+# def make_user():
+#     User.query.delete()
+#     Article.query.delete()
+#     UserWord.query.delete()
+#     PageReadEvent.query.delete()
+#     Vocabulary.query.delete()
 
-    username = "ziru"
-    password = "Test12345!"
+#     username = "ziru"
+#     password = "Test12345!"
 
-    # create a user
-    new_user = User(
-        username=username,
-    )
-    new_user.password_hash = password
-    db.session.add(new_user)
-    db.session.commit()
-    # print(new_user.id)
+#     # create a user
+#     new_user = User(
+#         username=username,
+#     )
+#     new_user.password_hash = password
+#     db.session.add(new_user)
+#     db.session.commit()
+#     # print(new_user.id)
 
-    # create a article
-    text = '''Maikaʻi loa! Ua aʻo nui kākou i ka ʻōlelo Hawaiʻi! I kēia manawa, e huakaʻi aku kākou i Lānaʻi, ka mokupuni hope loa o kā kākou huakaʻi.\n\
-Aloha kākou. ʻOiai e holo ana a puni nā moku nui ʻeono o Hawaiʻi nei, ua hiki mai i Lānaʻi, ka mokupuni hope o kā kākou huakaʻi mokupuni.\n\
-He liʻiliʻi wale nō kēia mokupuni: 18 mile ka loa, a he 13 mile ka laulā. He 8 mile ke kaʻawale mai Maui mai, a he 9 mai Molokaʻi mai hoʻi. ʻO Lānaʻihale ka inoa o ke kuahiwi nui loa o Lānaʻi, a ma kahi o ka 3,400 kapuaʻi kona kiʻekiʻe.\
-            '''.strip()
-    title = "Lānaʻi Story"
-    new_article = Article(
-            user_id=new_user.id,
-            uuid=str(uuid.uuid4()),
-            text=text,
-            title=title,
-            check_finished=False,
-    )
+#     # create a article
+#     text = '''Maikaʻi loa! Ua aʻo nui kākou i ka ʻōlelo Hawaiʻi! I kēia manawa, e huakaʻi aku kākou i Lānaʻi, ka mokupuni hope loa o kā kākou huakaʻi.\n\
+# Aloha kākou. ʻOiai e holo ana a puni nā moku nui ʻeono o Hawaiʻi nei, ua hiki mai i Lānaʻi, ka mokupuni hope o kā kākou huakaʻi mokupuni.\n\
+# He liʻiliʻi wale nō kēia mokupuni: 18 mile ka loa, a he 13 mile ka laulā. He 8 mile ke kaʻawale mai Maui mai, a he 9 mai Molokaʻi mai hoʻi. ʻO Lānaʻihale ka inoa o ke kuahiwi nui loa o Lānaʻi, a ma kahi o ka 3,400 kapuaʻi kona kiʻekiʻe.\
+#             '''.strip()
+#     title = "Lānaʻi Story"
+#     new_article = Article(
+#             user_id=new_user.id,
+#             uuid=str(uuid.uuid4()),
+#             text=text,
+#             title=title,
+#             check_finished=False,
+#     )
     
-    # new_article.users.id = new_user.id
-    db.session.add(new_article)
-    db.session.commit()
+#     # new_article.users.id = new_user.id
+#     db.session.add(new_article)
+#     db.session.commit()
 
-    date1 = datetime.datetime(2023, 3, 21, 0, 0, 0, 0)
-    date2 = datetime.datetime(2023, 3, 23, 0, 0, 0, 0)
-    date3 = datetime.datetime(2023, 4, 1, 0, 0, 0, 0)
-    date4 = datetime.datetime(2023, 4, 3, 0, 0, 0, 0)
-    date5 = datetime.datetime(2023, 4, 5, 0, 0, 0, 0)
-    date6 = datetime.datetime(2023, 4, 7, 0, 0, 0, 0)
-    date6 = datetime.datetime(2023, 4, 13, 0, 0, 0, 0)
-    date6 = datetime.datetime(2023, 4, 16, 0, 0, 0, 0)
-    date6 = datetime.datetime(2023, 4, 17, 0, 0, 0, 0)
-    date6 = datetime.datetime(2023, 4, 18, 0, 0, 0, 0)
-    date7 = datetime.datetime(2023, 4, 18, 8, 0, 0, 0)
-    page_events = [
-        PageReadEvent(user_id = new_user.id, date=date1, words_read=2650),
-        PageReadEvent(user_id = new_user.id, date=date1, words_read=2050),
-        PageReadEvent(user_id = new_user.id, date=date2, words_read=750),
-        PageReadEvent(user_id = new_user.id, date=date3, words_read=950),
-        PageReadEvent(user_id = new_user.id, date=date4, words_read=765),
-        PageReadEvent(user_id = new_user.id, date=date5, words_read=1265),
-        PageReadEvent(user_id = new_user.id, date=date6, words_read=980),
-        PageReadEvent(user_id = new_user.id, date=date7, words_read=1650),
-        PageReadEvent(user_id = new_user.id, words_read=250),
-    ]
-    db.session.add_all(page_events)
-    db.session.commit()
+#     date1 = datetime.datetime(2023, 3, 21, 0, 0, 0, 0)
+#     date2 = datetime.datetime(2023, 3, 23, 0, 0, 0, 0)
+#     date3 = datetime.datetime(2023, 4, 1, 0, 0, 0, 0)
+#     date4 = datetime.datetime(2023, 4, 3, 0, 0, 0, 0)
+#     date5 = datetime.datetime(2023, 4, 5, 0, 0, 0, 0)
+#     date6 = datetime.datetime(2023, 4, 7, 0, 0, 0, 0)
+#     date6 = datetime.datetime(2023, 4, 13, 0, 0, 0, 0)
+#     date6 = datetime.datetime(2023, 4, 16, 0, 0, 0, 0)
+#     date6 = datetime.datetime(2023, 4, 17, 0, 0, 0, 0)
+#     date6 = datetime.datetime(2023, 4, 18, 0, 0, 0, 0)
+#     date7 = datetime.datetime(2023, 4, 18, 8, 0, 0, 0)
+#     page_events = [
+#         PageReadEvent(user_id = new_user.id, date=date1, words_read=2650),
+#         PageReadEvent(user_id = new_user.id, date=date1, words_read=2050),
+#         PageReadEvent(user_id = new_user.id, date=date2, words_read=750),
+#         PageReadEvent(user_id = new_user.id, date=date3, words_read=950),
+#         PageReadEvent(user_id = new_user.id, date=date4, words_read=765),
+#         PageReadEvent(user_id = new_user.id, date=date5, words_read=1265),
+#         PageReadEvent(user_id = new_user.id, date=date6, words_read=980),
+#         PageReadEvent(user_id = new_user.id, date=date7, words_read=1650),
+#         PageReadEvent(user_id = new_user.id, words_read=250),
+#     ]
+#     db.session.add_all(page_events)
+#     db.session.commit()
 
-    # word tracker
-    word1 = "maikaʻi"
-    word2 = "loa"
-    word3 = "Lānaʻi"
-    word4= "ua"
+#     # word tracker
+#     word1 = "maikaʻi"
+#     word2 = "loa"
+#     word3 = "Lānaʻi"
+#     word4= "ua"
     
-    vocabs = [
-        Vocabulary(user_id=new_user.id, hawaiian_clean=word1, status=2), # known
-        Vocabulary(user_id=new_user.id, hawaiian_clean=word2, status=1), # studying
-        Vocabulary(user_id=new_user.id, hawaiian_clean=word3, status=3), # ignored
-        Vocabulary(user_id=new_user.id, hawaiian_clean=word4, status=2),
-    ]
-    db.session.add_all(vocabs)
-    db.session.commit()
+#     vocabs = [
+#         Vocabulary(user_id=new_user.id, hawaiian_clean=word1, status=2), # known
+#         Vocabulary(user_id=new_user.id, hawaiian_clean=word2, status=1), # studying
+#         Vocabulary(user_id=new_user.id, hawaiian_clean=word3, status=3), # ignored
+#         Vocabulary(user_id=new_user.id, hawaiian_clean=word4, status=2),
+#     ]
+#     db.session.add_all(vocabs)
+#     db.session.commit()
 
 
 def upload_dictionary():
@@ -132,5 +132,5 @@ def upload_dictionary():
 
 if __name__ == "__main__":
     with app.app_context():
-        make_user()
+        # make_user()
         upload_dictionary()
