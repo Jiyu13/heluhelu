9d56ddddf3a13ab7f6d0043190e1c79a3bd0c7c3
Ziru Zou <jiyucoding@yahoo.com>
2023-06-26 06:00:41 -0400

    create css styled  components for buttons and inputs elements

---
+++ client/src/components/Buttons.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const SubmitButtons = styled.input`
+    margin-top: 12px;
+    margin-right: 12px;
+    padding: 12px;
+    color: white;
+    font-weight: 700;
+    border-style: none;
+    border-radius: 8px;
+    background-color: #00B09B;
+    background-image: linear-gradient(to right, #00B09B, #96C93D);
+    opacity: 0.8;
+
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
+`
+
+export const ButtonButtons = styled.input`
+    margin-top: 12px;
+    margin-right: 12px;
+    width: 100px;
+    padding: 12px;
+    color: white;
+    font-weight: 700;
+    border-style: none;
+    border-radius: 8px;
+    background-color: #de801c;
+    opacity: 0.8;
+    
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
+`
+
+
+export const ButtonElements = styled.button`
+    border-style: none;
+    border-radius: 8px;
+    background-color: #00B09B;
+    background-image: linear-gradient(to right, #00B09B, #96C93D);
+    opacity: 0.8;
+    color: white;
+    font-weight: 700;
+    opacity: 0.8;
+
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
+`
