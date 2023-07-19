60287f687e2152f3ffae77a7d971e449e87d11a8
Ziru Zou <jiyucoding@yahoo.com>
2023-07-18 21:56:24 -0400

    change dropdown menu to just a clickable svg

--- client/src/components/DropDown.js
+++ client/src/components/DropDown.js
@@ -1,68 +1,65 @@
-import { Link } from "react-router-dom";
+// import more_icon from "../assets/images/more_horiz_white_48dp.svg"
+import info_white_48dp from "../assets/images/info_white_48dp.svg"
 
-import more_icon from "../assets/images/more_horiz_white_48dp.svg"
 import styled from "styled-components";
 import { useState } from "react";
 
 
 
 
-export function DropDown() {
+export function DropDown( {showInfo, setShowInfo} ) {
 
     const [showDropDown, setDropDown] = useState(false)
 
-    function handleDropDown() {
-        setDropDown(!showDropDown)
-    }
+    // function handleDropDown() {
+    //     setDropDown(!showDropDown)
+    // }
 
+    function handleShowInfo() {
+        setDropDown(!showDropDown)
+        setShowInfo(!showInfo)
+    }
 
     return (
         <MenuContainer>
-            {showDropDown && (
-                <EmptyDiv onClick={() => setDropDown(!showDropDown)}/>
-            )}
-
             <MenuTrigger>
-                <MenuImage src={more_icon} alt="more" onClick={handleDropDown}/>
+                <MenuImage src={info_white_48dp} alt="article info icon" onClick={handleShowInfo}/>
             </MenuTrigger>
-
-            {showDropDown ? 
-                <DropDownMenu>
-                    <MenuContent>
-                        <DropDownItem>
-                            <img alt=""></img>
-                            <LinkItem component={Link} to="">
-                                <TextWrapper>Vocab Stats</TextWrapper>
-                            </LinkItem>
-                        </DropDownItem>
-
-                        <DropDownItem>
-                            <img alt=""></img>
-                            <LinkItem component={Link} to="">
-                                <TextWrapper>Article Info</TextWrapper>
-                            </LinkItem>
-                        </DropDownItem>
-                    </MenuContent>
-
-                </DropDownMenu>
-                :
-                ""
-            }
-
         </MenuContainer>
+        // <MenuContainer>
+        //     {showDropDown && (
+        //         <EmptyDiv onClick={() => setDropDown(!showDropDown)}/>
+        //     )}
+
+        //     <MenuTrigger>
+        //         <MenuImage src={more_icon} alt="more" onClick={handleDropDown}/>
+        //     </MenuTrigger>
+
+        //     {showDropDown ? 
+        //         <DropDownMenu>
+                    
+        //             <MenuContent>
+        //                 <DropDownItem onClick={handleShowInfo}>
+                            
+        //                     <LinkItem>
+        //                         <TextWrapper>Article Info</TextWrapper>
+        //                     </LinkItem>
+        //                 </DropDownItem>
+        //             </MenuContent>
+
+        //         </DropDownMenu>
+        //         :
+        //         ""
+        //     }
+        // </MenuContainer>
     )
 }
 
-const EmptyDiv = styled.div`
-    width: 100%; 
-    height: 100%;
-    position: fixed;
-`
 
 const MenuContainer = styled.div`
-    position: relative;
-    vertical-align: top;
-    height: 23px;
+    // position: relative;
+    // vertical-align: top;
+    // height: 23px;
 `
 const MenuTrigger = styled.div`
     display: grid;
@@ -79,60 +76,65 @@ const MenuImage = styled.img`
         cursor: pointer;
     }
 `
+// const EmptyDiv = styled.div`
+//     width: 100%; 
+//     height: 100%;
+//     position: fixed;
+// `
 
-const DropDownMenu = styled.div`
-    display: block;
-    background-color: white;
-    left: auto;
-    right: 0;
-    min-width: 8rem;
-    padding-left: 0;
-    padding-right: 0;
-    margin-right: 10px;
-    position: absolute;
-    z-index: 20;
-    border-radius: 8px;
-    grid-column-start: 7;
-`
+// const DropDownMenu = styled.div`
+//     display: block;
+//     background-color: white;
+//     left: auto;
+//     right: 0;
+//     min-width: 8rem;
+//     padding-left: 0;
+//     padding-right: 0;
+//     margin-right: 10px;
+//     position: absolute;
+//     z-index: 20;
+//     border-radius: 8px;
+//     grid-column-start: 7;
+// `
 
-const MenuContent = styled.div`
-    border-radius: 1rem;
-    // padding-bottom: 0.5rem;
-    // padding-top: 0.5rem;
-    text-align: center;
-`
+// const MenuContent = styled.div`
+//     border-radius: 1rem;
+//     // padding-bottom: 0.5rem;
+//     // padding-top: 0.5rem;
+//     text-align: center;
+// `
 
-const DropDownItem = styled.div`
-    // display: block;
-    justify-content: center;
-    align-items: center;
-    font-size: .875rem;
-    line-height: 1.5;
-    padding: 1rem 1rem;
-    position: relative;
-    &:hover {
-        cursor: pointer;
-        background-image: linear-gradient(to right, #00B09B, #96C93D);;
-    }
-    &:first-child {
-        border-radius: 8px 8px 0 0;
-    }
+// const DropDownItem = styled.div`
+//     // display: block;
+//     justify-content: center;
+//     align-items: center;
+//     font-size: .875rem;
+//     line-height: 1.5;
+//     padding: 1rem 1rem;
+//     position: relative;
+//     &:hover {
+//         cursor: pointer;
+//         background-image: linear-gradient(to right, #00B09B, #96C93D);;
+//     }
+//     // &:first-child {
+//     //     border-radius: 8px 8px 0 0;
+//     // }
 
-    &:last-child {
-        border-radius: 0 0 8px 8px;
-    }
-`
+//     // &:last-child {
+//     //     border-radius: 0 0 8px 8px;
+//     // }
+// `
 
-const LinkItem = styled.a`
-    text-decoration: none;
-    margin: 0 10px;
-    color: black;
-    opacity: 0.7;
-    &:hover {
-        opacity: 1;
-        cursor: pointer;    
-    }
+// const LinkItem = styled.div`
+//     text-decoration: none;
+//     margin: 0 10px;
+//     color: black;
+//     opacity: 0.7;
+//     &:hover {
+//         opacity: 1;
+//         cursor: pointer;    
+//     }
     
-`
+// `
 
-const TextWrapper = styled.span``
+// const TextWrapper = styled.span``
