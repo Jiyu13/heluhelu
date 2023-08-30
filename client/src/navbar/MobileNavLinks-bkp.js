a67e14d294c27ef9b51d8665f1d8ebf4e8768391
Ziru Zou <jiyucoding@yahoo.com>
2023-08-30 13:01:53 -0400

    style dropdown menu for mobile version

---
+++ client/src/navbar/MobileNavLinks-bkp.js
@@ -0,0 +1,128 @@
+// render the mobile view with a animated hamburger menu when screensize is smaller
+
+// framer motion
+
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+import { MenuToggle } from "./MenuToggle";
+import { Accessibility } from "./Accessiblity";
+import { UserContext } from "../components/UserContext";
+
+
+
+export function MobileNavLinks() {
+    // state to toggle the changes, when click show the actual ul menu, otherwise hidden by default
+    const [isOpen, setOpen] = useState(false)
+
+    const {firstArticle, article} = useContext(UserContext)
+    const currentArticle = article !== null ? article : firstArticle
+
+    return (
+        <NavLinksContainer>
+            <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)}/>
+
+            {/* isOpen=true, grab LinksWrapper*/}
+            {isOpen && (
+                <LinksWrapper>
+                    <LinkItem>
+                        <Link href="/">Home</Link>
+                    </LinkItem>
+
+                    <LinkItem>
+                        <Link href="/stats">My Stats</Link>
+                    </LinkItem>
+
+                    <LinkItem>
+
+                        {/* {currentArticle ? 
+                            <Link 
+                                className="recent-reading"
+                                href={`/articles/${currentArticle?.id}`}
+                            >
+                                Currently Reading
+                                <HideTitle className="last-open-title">{currentArticle?.title}</HideTitle>
+                            </Link>
+                            :
+                            <Link className="recent-reading" style={{cursor: "pointer"}}>
+                                Currently Reading
+                                <HideTitle className="last-open-title">No current reading.</HideTitle>
+                            </Link>
+                        }  */}
+                            <Link 
+                                href={`/articles/${currentArticle?.id}`}
+                            >
+                                Currently Reading
+                            </Link>
+                       </LinkItem>
+
+                    <Marginer/>
+                    <Accessibility/>
+                </LinksWrapper>
+
+            )}
+
+            
+        </NavLinksContainer>
+    )
+}
+
+
+export const NavLinksContainer = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    // z-index: 1;
+`;
+
+const LinksWrapper = styled.ul`
+    position: absolute;
+    top: 45px;
+    right: 10px;
+    background-color: #ced6e0;
+    border-radius: 8px;
+    padding: 10px 20px;
+    width: 150px;
+    height: 250px;
+    box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19) !important;
+
+    &:before {
+        content: "";
+        position: absolute;
+        top: -5px;
+        right: 20px;
+        height: 20px;
+        width: 20px;
+        background: var(--secondary-bg);
+        transform: rotate(45deg);
+    }
+`;
+
+// render a single link, click and redirect to a specific page
+const LinkItem = styled.li`
+    width: 100%;
+    padding: 0.5rem 0;
+    color: #222;
+    font-weight: 500;
+    font-size: 1rem;
+    display: flex;
+    flex-direction: column;
+    margin: 10px auto;
+
+    &:hover {
+        // border-bottom: 0.5px solid black;
+        // width: 90%;
+    }
+`;
+
+const Link = styled.a`
+    text-decoration: none;
+    color: inherit;
+    font-size: inherit;
+`;
+
+const Marginer = styled.div`
+    margin-top: 60px;
+    border-top: 1px solid black;
+    // rgba(255, 255, 255, 0.2);
+    height: 1rem;
+`;
--- client/src/navbar/MobileNavLinks.js
+++ client/src/navbar/MobileNavLinks.js
@@ -1,134 +1,157 @@
-// render the mobile view with a animated hamburger menu when screensize is smaller
-
-// framer motion
-
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { MenuToggle } from "./MenuToggle";
-import { Accessibility } from "./Accessiblity";
-import { UserContext } from "../components/UserContext";
 
+import { useContext } from 'react';
+import { UserContext } from '../components/UserContext';
+import apiFetch from "../api/ApiFetch";
+import { useNavigate } from "react-router-dom";
 
+import { DropdownItem } from "../profile/DropdownItem";
+// import settings_black_24dp from "../assets/images/settings_black_24dp.svg"
+import account_circle_black_24dp from "../assets/images/account_circle_black_24dp.svg"
+import logout_black_24dp from "../assets/images/logout_black_24dp.svg"
+import person_black_24dp from "../assets/images/person_black_24dp.svg"
+import home_black_24dp from "../assets/images/home_black_24dp.svg"
+import equalizer_black_24dp from "../assets/images/equalizer_black_24dp.svg"
+import book_black_24dp from "../assets/images/book_black_24dp.svg"
 
 export function MobileNavLinks() {
-    // state to toggle the changes, when click show the actual ul menu, otherwise hidden by default
-    const [isOpen, setOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const {setUser, user, firstArticle, article} = useContext(UserContext)
 
-    const {firstArticle, article} = useContext(UserContext)
     const currentArticle = article !== null ? article : firstArticle
 
+
+    let navigate = useNavigate()
+    function redirectHome() {
+        navigate('/') 
+    }
+
+    function handleClick() {
+      setIsOpen(!isOpen)
+    }
+
+    // =========== logout =================================
+    function handleLogout() {
+      apiFetch('/logout', {
+        method: "DELETE"
+      })
+      .then(() => {
+        setUser(null)
+        redirectHome()
+      })
+    }
+
+    // ============ settings ============================
+    // function handleSettings() {
+    //   return
+    // }
+
+    // ========== profile ===============================
+    function handleProfile() {
+      return
+    }
     return (
         <NavLinksContainer>
-            <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)}/>
+            <MenuTrigger onClick={handleClick}>
+              <ProfileImg src={account_circle_black_24dp} alt="profile image"></ProfileImg>
+            </MenuTrigger>
 
-            {/* isOpen=true, grab LinksWrapper*/}
             {isOpen && (
-                <LinksWrapper>
-                    <LinkItem>
-                        <Link href="/">Home</Link>
-                    </LinkItem>
-
-                    <LinkItem>
-                        <Link href="/stats">My Stats</Link>
-                    </LinkItem>
-
-                    <LinkItem>
-
-                        {/* {currentArticle ? 
-                            <Link 
-                                className="recent-reading"
-                                href={`/articles/${currentArticle?.id}`}
-                            >
-                                Currently Reading
-                                <HideTitle className="last-open-title">{currentArticle?.title}</HideTitle>
-                            </Link>
-                            :
-                            <Link className="recent-reading" style={{cursor: "pointer"}}>
-                                Currently Reading
-                                <HideTitle className="last-open-title">No current reading.</HideTitle>
-                            </Link>
-                        }  */}
-                            <Link 
-                                href={`/articles/${currentArticle?.id}`}
-                            >
-                                Currently Reading
-                            </Link>
-                       </LinkItem>
-
-                    <Marginer/>
-                    <Accessibility/>
-                </LinksWrapper>
-
+              <MenuDropdownList>
+                <UserName>{user?.username}</UserName>
+                <hr style={{backgroundColor: "#252525"}}/>
+                <DropdownItem 
+                  icon={home_black_24dp} 
+                  icon_info="homepage-icon" 
+                  text="Home" 
+                  handleProfileClick={handleProfile}
+                  goTo="/"
+                />
+                <DropdownItem 
+                  icon={equalizer_black_24dp} 
+                  icon_info="my stats icon" 
+                  text="My Stats" 
+                  handleProfileClick={handleProfile}
+                  goTo="/stats"
+                />
+                <DropdownItem 
+                  icon={book_black_24dp} 
+                  icon_info="currently reading icon" 
+                  text="Currently Reading" 
+                  handleProfileClick={handleProfile}
+                  goTo={`/articles/${currentArticle?.id}`}
+                />
+                <DropdownItem 
+                  icon={person_black_24dp} 
+                  icon_info="profile icon" 
+                  text="Your Profile" 
+                  handleProfileClick={handleProfile}
+                  goTo="/profile"
+                />
+                {/* <DropdownItem icon={settings_black_24dp} icon_info="settings icon" text="Settings" handleMenuItemClick={handleSettings}/> */}
+                <DropdownItem 
+                  icon={logout_black_24dp} 
+                  icon_info="logout icon" 
+                  text="Log out" 
+                  handleMenuItemClick={handleLogout}
+                  goTo="/logout"
+                />
+              </MenuDropdownList>
             )}
-
-            
         </NavLinksContainer>
     )
 }
 
-// const HideTitle = styled.div`
-//     display: none;
-//     // text-align: center;
-//     flex-wrap: pre-wrap;
-//     font-size: 14px;
-// `
+const UserName = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 0.8rem auto 1.2rem;
+`
+
+const MenuDropdownList = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  top: 55px;
+  right: 15px;
+  background-color: #ced6e0;
+  border-radius: 8px;
+  padding: 10px 20px;
+  // height: 250px;
+  box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19) !important;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: -5px;
+    right: 8px;
+    height: 20px;
+    width: 20px;
+    background: var(--secondary-bg);
+    transform: rotate(45deg);
+  }
+`
+const ProfileImg = styled.img`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  transition: transform .2s;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+`
+const MenuTrigger = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`
 
 export const NavLinksContainer = styled.div`
     height: 100%;
     display: flex;
     align-items: center;
     // z-index: 1;
-`;
-
-const LinksWrapper = styled.ul`
-    position: absolute;
-    top: 45px;
-    right: 10px;
-    background-color: #ced6e0;
-    border-radius: 8px;
-    padding: 10px 20px;
-    width: 150px;
-    height: 250px;
-    box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19) !important;
-
-    &:before {
-        content: "";
-        position: absolute;
-        top: -5px;
-        right: 20px;
-        height: 20px;
-        width: 20px;
-        background: var(--secondary-bg);
-        transform: rotate(45deg);
-    }
-`;
-
-// render a single link, click and redirect to a specific page
-const LinkItem = styled.li`
-    width: 100%;
-    padding: 0.5rem 0;
-    color: #222;
-    font-weight: 500;
-    font-size: 1rem;
-    display: flex;
-    flex-direction: column;
-    margin: 10px auto;
-
-    &:hover {
-        // border-bottom: 0.5px solid black;
-        // width: 90%;
-    }
-`;
-
-const Link = styled.a`
-    text-decoration: none;
-    color: inherit;
-    font-size: inherit;
-`;
-
-const Marginer = styled.div`
-    margin-top: 60px;
-    border-top: 1px solid black;
-    // rgba(255, 255, 255, 0.2);
-    height: 1rem;
 `;
