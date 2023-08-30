76a73c328f6e80e36fc411a113c6a6c4af405990
Ziru Zou <jiyucoding@yahoo.com>
2023-08-30 12:59:00 -0400

    clean code

--- client/src/navbar/Accessiblity.js
+++ client/src/navbar/Accessiblity.js
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 import { DropdownItem } from "../profile/DropdownItem";
 import { NavLinksContainer } from "./MobileNavLinks";
-import settings_black_24dp from "../assets/images/settings_black_24dp.svg"
+// import settings_black_24dp from "../assets/images/settings_black_24dp.svg"
 import account_circle_black_24dp from "../assets/images/account_circle_black_24dp.svg"
 import logout_black_24dp from "../assets/images/logout_black_24dp.svg"
 import person_black_24dp from "../assets/images/person_black_24dp.svg"
@@ -37,19 +37,8 @@ export function Accessibility() {
       })
     }
 
-    // ============ settings ============================
-    function handleSettings() {
-      return
-    }
-
-    // ========== profile ===============================
-    function handleProfile() {
-      return
-    }
     return (
         <NavLinksContainer>
-            {/* <ShowAccount>{user.username}</ShowAccount>
-            <LogoutButton onClick={handleLogout}>Logout</LogoutButton>  */}
             <MenuTrigger onClick={handleClick}>
               <ProfileImg src={account_circle_black_24dp} alt="profile image"></ProfileImg>
             </MenuTrigger>
@@ -62,7 +51,6 @@ export function Accessibility() {
                   icon={person_black_24dp} 
                   icon_info="profile icon" 
                   text="Your Profile" 
-                  handleProfileClick={handleProfile}
                   goTo="/profile"
                 />
                 {/* <DropdownItem icon={settings_black_24dp} icon_info="settings icon" text="Settings" handleMenuItemClick={handleSettings}/> */}
@@ -94,7 +82,6 @@ const MenuDropdownList = styled.div`
   background-color: #ced6e0;
   border-radius: 8px;
   padding: 10px 20px;
-  width: 150px;
   // height: 250px;
   box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19) !important;
 
@@ -128,52 +115,3 @@ const MenuTrigger = styled.div`
 
 `
 
-
-
-const ShowAccount = styled.div`
-    margin-right: 5px;
-    border: 0;
-    outline: 0;
-    padding: 8px 1em;
-    color: #fff;
-    font-size: 13px;
-    font-weight: 600;
-    border-radius: 20px;
-    background-color: transparent;
-    background-image: linear-gradient(to right, #00B09B, #96C93D);
-    transition: all 240ms ease-in-out;
-    cursor: pointer;
-`
-
-
-const AccessibilityContainer = styled.div`
-    display: flex;
-    margin-left: 10px;
-`;
-
-const LogoutButton = styled.div`
-    border: 0;
-    outline: 0;
-    padding: 8px 1em;
-    color: #222;
-    font-size: 13px;
-    font-weight: 600;
-    border-radius: 20px;
-    background-color: transparent;
-    border: 2px solid #00B09B;
-    transition: all 240ms ease-in-out;
-    cursor: pointer;
-
-
-    // transition color from #6adf76 to #00c9ff
-    &:hover {
-      color: #fff;
-      background-color: #00B09B;
-      background-image: linear-gradient(to right, #00B09B, #96C93D);
-    }
-
-    // apply the styling for all button except for the last one
-    &:not(:last-of-type) {
-      margin-right: 7px;
-    }
-`;
