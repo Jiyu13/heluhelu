56ff56349f9d9b9b6f5332f520dca13f8cf2d9f8
Ziru Zou <jiyucoding@yahoo.com>
2024-09-13 09:32:39 -0400

    import custom styled components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,7 +11,7 @@ import apiFetch from "../api/ApiFetch";
 
 import { SkeletonHomePage } from "../skeleton-screens/SkeletonHomePage";
 import { UserContext } from "./UserContext";
-import { PageContainer } from "../styles/Container";
+import { PageContainer, PageText, PageTitle } from "../styles/Container";
 import { LoginPage } from "../account/LoginPage";
 
 
@@ -54,8 +54,8 @@ export function Home({ articles, setArticles, onDeleteArticle}) {
                         />
                     )} 
 
-                    <HomepageTitle>Heluhelu</HomepageTitle>
-                    <HomepageText>Load your Hawaiian texts and get started reading! Click on words you don't know to see their definitions and keep track of your vocabulary as you read!</HomepageText>
+                    <PageTitle>Heluhelu</PageTitle>
+                    <PageText>Load your Hawaiian texts and get started reading! Click on words you don't know to see their definitions and keep track of your vocabulary as you read!</PageText>
 
                     <HomepageButtonContainer>
                         <Link to={"/import/text"}>
@@ -98,30 +98,7 @@ const ButtonSpan = styled.span`
     }
 `
 
-const HomepageTitle = styled.h1`
-    margin-block-start: 0.67em;
-    margin-block-end: 0.67em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-`
-
-const HomepageText = styled.div`
-    max-width: 700px;
-    text-align: center;
-    margin: 18px auto 18px auto;
-    font-size: 16px;
-    color: #999!important;
-    line-height: 1.6;
-    display: block;
-    font-family: readex pro,arial,sans-serif;
-`
-
 const HomepageButtonContainer = styled.div`
     display: block;
     text-align: center;
 `
-// const PageContainer = styled.div`
-//     padding-top: 90px;
-//     text-align: center;
-// `
