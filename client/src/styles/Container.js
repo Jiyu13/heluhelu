9e2b8f9780398951f55b4c1f3724ed03e38a0570
Ziru Zou <jiyucoding@yahoo.com>
2024-09-13 05:28:26 -0400

    add pageTitle

--- client/src/styles/Container.js
+++ client/src/styles/Container.js
@@ -6,6 +6,14 @@ export const PageContainer = styled.div`
     margin: 0 auto;
 `
 
+export const PageTitle = styled.h1`
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+`
+
 export const MainContainer = styled.main`
     min-height: 100vh;
 `
